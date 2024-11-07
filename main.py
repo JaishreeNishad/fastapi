4288e5def280026f414d357b86283264593cb71a
Jaishree Nishad <jaysh2222@gmail.com>
2024-11-07 16:19:53 +0530

    create request body

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 from fastapi import FastAPI
-from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/first")
-def index():
-    return {"message": "Hello World"}
+db = {}
+
+
+class Item(BaseModel):
+    name: str
+    desc: str
+
+
+@app.post("/")
+def create(item: Item):
+    db[item.name] = item.desc
+    return {"Item": item}
+
+@app.get("/")
+def get_all_data():
+    return db
+
+# @app.get("/first")
+# def index():
+#     return {"message": "Hello World"}
 
 #http://myntra.com/items/item_id 
 #example of path parameter
 
-@app.get("/items/{item_id}")
-def index(item_id :int=0): # apply validate (item_id: int)
-         return {"producut_id":item_id}
+# @app.get("/items/{item_id}")
+# def index(item_id :int=0): # apply validate (item_id: int)
+#          return {"producut_id":item_id}
      
-#query parameter
+# #query parameter
 
-@app.get("/items/")
-def index(q:int):
-    return {"product is":q}
+# @app.get("/items/")
+# def index(q:int,m:Optional[int]=10): #optional parameter
+#     return {"product is":q,"m":m}
+
+
+# @app.get("/items/{file_path:path}")
+# def index(file_path:str):
+#     return{"file_path":file_path}
+
+# 
+
